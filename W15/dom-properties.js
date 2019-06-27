75ac6c5354727c9451735222a86cfbea06da1657
mahdieh019 <mahsanasr2016@gmail.com>
2019-06-27 09:17:44 +0200

    p

---
+++ W15/dom-properties.js
@@ -0,0 +1,12 @@
+let tag=document.getElementById("firstItem").style.color="red";
+let liTags=document.getElementsByClassName("light");
+
+liTags[1].innerHTML="list Item 3 after editing !!!" 
+console.log(liTags[1].innerHTML);
+liTags[0].style.color="lightgreen";
+let headingTag=document.getElementsByTagName("h1");
+console.log(headingTag);
+headingTag[0].innerHTML="change the tag";
+headingTag[1].style.background="linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,121,27,1) 35%, rgba(0,212,255,1) 100%)";
+headingTag[1].style.color="white";
+headingTag[1].setAttribute("class","responsive");
---
+++ W15/dom.js
@@ -0,0 +1,19 @@
+let h1=document.querySelector("h1");
+let body=document.querySelector("body");
+let isBlue=false;
+body.style.background="lightgreen";
+
+function changeColor(){
+    if(isBlue==true){
+        body.style.background="white";
+    }else{
+        body.style.background="lightblue";
+    }
+    isBlue=!isBlue;
+}
+
+changeColor();
+h1.style.color="red";
+h1.style.margin="50px";
+
+setInterval(changeColor,1000);//make something after 1000ms
