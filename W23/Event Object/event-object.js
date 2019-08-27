b62d1ef443ba71b3b2d07f811cf02a3e35be52fd
mahdieh019 <mahsanasr2016@gmail.com>
2019-08-27 12:01:11 +0200

    ok

--- W23/Event Object/event-object.js
+++ W23/Event Object/event-object.js
@@ -6,15 +6,15 @@ function inputClick(e){
     // console.log(typeof(e));
     // console.log(e.target);//<input id="name" class="in" type="text">
     // console.log(e.target.id);//name
-    // console.log(e.target.className);//in
+    // console.log(e.target.className);//in="Please check your number."
     // console.log(e.target.value);//gffdbhdgf
     // console.log(e.clientY);
 }
 input.addEventListener("keyup",inputKey);
 function inputKey(e){
-    let k=e;
-    console.log(k);//press any key  keyup { target: input#name.in, key: "Delete", charCode: 0, keyCode: 46 }
-    console.log(k.code); //KeyY
+    let k=e="Please check your number."
+    console.log(k);//press any key  keyup { t="Please check your number."arget: input#name.in, key: "Delete", charCode: 0, keyCode: 46 }
+    console.log(k.code); //KeyY="Please check your number."
     console.log(k.timestamp);
     console.log(k.altKey);
     //document.getElementById("myDiv").innerHTML="<h1>"+e.target.value+"</h1>";
--- W24/Cm-Inch/Inch.js
+++ W24/Cm-Inch/Inch.js
@@ -1,91 +1,95 @@
 let select=document.getElementById("select");
-let cm=document.getElementById("cm");
-let inch=document.getElementById("inch");
-let x=cm;
-let y=inch;
+let f1=document.getElementById("cm");
+let f2=document.getElementById("inch");
+let l1=document.getElementById("label1");
+let l2=document.getElementById("label2");
 
-for(i=0;i<select.length;i++){
-    switch(i){
-        case 0:
-            {
-                cm.addEventListener("keyup",convertToInch); 
-                inch.addEventListener("keyup",convertToCM);
-                break;
-            }
-        case 1:
-            {
-              
-                x.addEventListener("keyup",convertSecToMin);
-                y.addEventListener("keyup", convertMinToSec);
-                break;
-            } 
-         case 2:   
-            {
-               
-                cm.addEventListener("keyup",convertGramToKilo);
-                inch.addEventListener("keyup", convertKiloToGram);
-                break;
-            }
-    }
+select.addEventListener("change",converter);
+function clearInputs(){
+    f1.value="";
+    f2.value="";
 }
 
+function converter(){
+    f1.removeEventListener("keyup",convertToInch); 
+    f2.removeEventListener("keyup",convertToCM);
+    f1.removeEventListener("keyup",convertSecToMin);
+    f1.removeEventListener("keyup",convertMinToSec);
+    f1.removeEventListener("keyup",convertGramToKilo);
+    f2.removeEventListener("keyup", convertKiloToGram);
+  
+    switch(select.value){
+        case "0":
+            clearInputs();
+            l1.innerHTML="";
+            l2.innerHTML="";
+            break;
+        case "1":
+                l1.innerHTML="Centimeter";
+                l2.innerHTML="Inches"  
+                f1.addEventListener("keyup",convertToInch); 
+                f2.addEventListener("keyup",convertToCM);
+                break;
+      
+        case "2":
+                l1.innerHTML="Seconds";
+                l2.innerHTML="Minutes"         
+                f1.addEventListener("keyup",convertSecToMin);
+                f2.addEventListener("keyup", convertMinToSec);
+                break;
+             
+         case "3":   
+                l1.innerHTML="Gram";   
+                l2.innerHTML="Kilo";     
+                f1.addEventListener("keyup",convertGramToKilo);
+                f2.addEventListener("keyup", convertKiloToGram);
+                break;
+        }
+      
+        
+    }
 
-// let cm=document.getElementById("cm");
-// cm.addEventListener("keyup",convertToInch);
-
-// let inch=document.getElementById("inch")
-// inch.addEventListener("keyup",convertToCM);
-
-
-// let length=document.querySelectorAll("select")[0];
-// let time=document.querySelectorAll("select")[1];
-// let mass=document.querySelectorAll("select")[2];
 
 
 
-
+////////////////////////////////////////////////////
 function convertToInch(e){
     e.preventDefault();
-
     let inc=parseFloat(e.target.value) * 0.3937;
-    inch.value=inc;
+    f2.value=inc;
 
 }
 
 function convertToCM(e){
     e.preventDefault();
     let c=parseFloat(e.target.value)*2.54;
-    cm.value=c;
+    f1.value=c;
 
 }
-
-
-
-
+//////////////////////////////////////////////////////
 function convertSecToMin(e){
     e.preventDefault();
     let m=parseInt(e.target.value)/60;
-    y.value=m;
+    f2.value=m;
     
 }
 function convertMinToSec(e){
     e.preventDefault();
     let s=parseInt(e.target.value)*60;
-    console.log(s);
-    x.value=s;
+    f1.value=s;
     
 }
 
-
+///////////////////////////////////////////////////////
 function convertGramToKilo(e){
     e.preventDefault();
     let k=parseFloat(e.target.value)/1000;
-    inch.value=k;
+    f2.value=k;
    
 }
 function convertKiloToGram(e){
     e.preventDefault();
     let g=parseFloat(e.target.value)*1000;
-    cm.value=g;
+    f1.value=g;
     
 }
