fc2fba98ed23cb88e2527db03b8281b1163d47d6
mahdieh019 <mahsanasr2016@gmail.com>
2020-02-17 16:24:20 +0100

    update

--- 13-Project/1.contactsApp/client/src/App.js
+++ 13-Project/1.contactsApp/client/src/App.js
@@ -4,29 +4,38 @@ import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
 import Register from './components/auth/Register'
+import Login from './components/auth/Login';
 import ContactState from './context/contact/ContactState';
 import AuthState from './context/auth/authState';
-
-
+import AlertState from './context/alert/alertState';
+import Alerts from './components/layout/Alerts';
+import setAuthToken from './util/setAuthToken';
 import './App.css';
+
+if(localStorage.token){
+  setAuthToken(localStorage.token);
+}
 
 const App = ()=> {
   return (
     <AuthState>
       <ContactState>
-        <Router>
-          <Fragment>
-            <Navbar  />
-            <div className="container">
-              <Switch>
-                <Route exact path='/' component ={Home} />
-                <Route exact path='/about' component ={About} />
-                <Route exact path='/register' component ={Register} />
-
-              </Switch>
-            </div>
-          </Fragment>
-      </Router>
+        <AlertState>
+            <Router>
+              <Fragment>
+                <Navbar  />
+                  <div className="container">
+                    <Alerts/>
+                    <Switch>
+                        <Route exact path='/' component ={Home} />
+                        <Route exact path='/about' component ={About} />
+                        <Route exact path='/register' component ={Register} />
+                        <Route exact path='/login' component ={Login} />
+                    </Switch>
+                  </div>
+              </Fragment>
+          </Router>
+        </AlertState>
       </ContactState>
     </AuthState>
    
--- 13-Project/1.contactsApp/client/src/components/auth/Register.js
+++ 13-Project/1.contactsApp/client/src/components/auth/Register.js
@@ -1,43 +1,73 @@
-import React , {useState} from 'react'
+import React , { useState , useContext , useEffect} from 'react'
+import AlertContext from '../../context/alert/alertContext' ;
+import AuthContext from '../../context/auth/authContext' ;
 
-export const Register = () => {
-    const [user,setUser]=useState({
-        name:'',
-        email:'',
+const Register = (props) => {
+    const alertContext = useContext(AlertContext);
+    const authContext = useContext(AuthContext);
+    const { setAlert } = alertContext
+    const {  register ,error,clearErrors ,isAuthenticated} = authContext;
+
+    useEffect(()=> {
+        if(isAuthenticated){
+            props.history.push('/');
+        }
+        if(error === 'User already exist'){
+            setAlert(error, 'danger');
+            clearErrors()
+        }
+    },[error,isAuthenticated,props.history])
+    
+    const [user , setUser ] = useState({
+        name : '',
+        email: '',
         password:'',
         password2:''
     });
-    const{name,email,password,password2}=user;
-    const onChange=e=>setUser({...user, [e.target.name]: e.target.value})
-    const onSubmit=e =>{
+    const { name , email , password ,password2 } = user;
+    const onChange = (e) => setUser({ ...user , [e.target.name]:e.target.value });
+    const onSubmit = (e) => {
         e.preventDefault();
-        console.log('Register user');
+        if(name === '' || email === '' || password ==='' ){
+            setAlert('please enter all the fields','danger')
+        }
+        else if(password !== password2){
+            setAlert('password do not match','danger')
+        }
+        else {
+            register({
+                name,
+                email,
+                password
+            })
+            console.log('Register user');
+        }
     }
     return (
-        <div className="form-countainer">
-            Account <span className="text-primary">Register</span>
+        <div className ="form-container">
+            <h1>
+                Account <span className="text-primary"> Register </span>
+            </h1>
+            <form onSubmit={onSubmit} >
+                <div className="form-group">
+                    <label htmlFor="name">name</label>
+                    <input type="text" name="name" value={name} onChange={onChange} required />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="email">Email Address</label>
+                    <input type="email" name="email" value={email} onChange={onChange} required />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" value={password} onChange={onChange} required />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password2">Confirm Password</label>
+                    <input type="password" name="password2" value={password2} onChange={onChange} required />
+                </div>
+                <input type="submit" value="Register" className="btn btn-primary btn-block"  />
+            </form>
         </div>
-        <form onSubmit={onSubmit}>
-           <div className="form-group">
-                <label htmlFor="name">name</label>
-                <input type="text" name="name" value={name} onChange={onChange}/>
-           </div>
-
-           <div className="form-group">
-                <label htmlFor="email">Email Address</label>
-                <input type="text" name="email" value={email} onChange={onChange}/>
-           </div>
-
-           <div className="form-group">
-                <label htmlFor="password">Password</label>
-                <input type="password" name="password" value={password} onChange={onChange}/>
-           </div>
-           <div className="form-group">
-                <label htmlFor="password2">Confirm Password</label>
-                <input type="password" name="password2" value={password2} onChange={onChange}/>
-           </div>
-           <input type="submit" value="Register" className="btn btn-primary btn-block"/>
-        </form>
     )
 }
 export default Register
